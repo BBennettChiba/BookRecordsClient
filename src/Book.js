3943e2355a082c74886ce33892d5e27fade905db
BBennettChiba <BrysonCBennett@gmail.com>
2021-03-24 10:46:40 +0900

    formatting stuff

--- src/Book.js
+++ src/Book.js
@@ -5,22 +5,16 @@ import './App.css'
 export default function Book({info, setFocusedBook, setFocused, focused}) {
     const [thisBook, setThisBook] = useState([])
     const [thisImg, setThisImg] = useState('')
-    // const [thisAuthor, setThisAuthor] = useState([])
-    // const [clickedOn, setClickedOn] = useState([false]);
-    // const [thisDescription, setThisDescription] = useState('')
-    
+
     useEffect(()=> {
         async function APIcall (){
             let googleBook = await 
             // axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${info}*/&key=${process.env.REACT_APP_GOOGLE_KEY}*/`).then(res=>res.data);
             axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${info}`).then(res=>res.data);
             if (googleBook.totalItems === 0) return;
-            // console.log(googleBook);
             let book = googleBook.items[0].volumeInfo;
             setThisImg(<img src={book.imageLinks.thumbnail} alt={book.title}/>)
-            // setThisAuthor(book.authors);
             setThisBook(book)
-            // setThisDescription(book.description)
         }
         APIcall()
     }, [info])
@@ -32,12 +26,6 @@ export default function Book({info, setFocusedBook, setFocused, focused}) {
 
     return (<>
         <div className="thumbnail" onClick={changeFocus}>{thisImg}</div>
-        {/* {!clickedOn && <div className="book" onClick={()=>setClickedOn(!clickedOn)}>
-            <div><b>Title</b> {thisBook.title}</div>
-            <div><b>Author</b> {thisAuthor}</div>
-            {thisImg}
-            <div><b>Description</b>{thisDescription}</div>
-        </div>} */}
         </>
     )
 }
